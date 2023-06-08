c3b23b79ae65fc31131c15185669f9fd98fffb30
Vergel dela Cruz <c0835208@mylambton.ca>
2023-06-08 10:11:06 -0400

    use postgre

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddScoped<IArtistService, ArtistService>();
 
 // Use SQL Server connection string
  builder.Services.AddDbContext<AppDBContext>(
-    options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
